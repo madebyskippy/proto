bd019d6d112662d6a4902a72c58788fb5c1e3e50
madebyskippy <timothys@nyu.edu>
2017-02-15 15:19:12 -0500

    startin the meat stuffs

---
+++ Assets/meatfight/meatcontrol.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class meatcontrol : MonoBehaviour {
+	[SerializeField] Text fill;
+
+	[SerializeField] GameObject manager;
+	private mf_manager managers;
+
+	/*
+	 * if the punchtimer is on, then you can do the actual gameplay
+	 * --take in input, fill in the "fill"
+	 * --if the fill is full, send message to manager
+	 * 
+	 * else you can just flop around
+	 * --take in input, do nothing (but diff action than if you are filling)
+	 * 
+	 */
+
+
+	// Use this for initialization
+	void Start () {
+		fill.text = "";
+		managers = manager.GetComponent<mf_manager> ();
+		Debug.Log (65.ToString ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for (int i = 0; i < 26; i++) {
+
+			if (Input.GetKeyDown (KeyCode.A + i)) {
+				if (managers.getIsPunching ())
+					meatfill ((char)(97 + i));
+				else
+					wiggle ();
+			}
+
+		}
+	}
+
+	void wiggle(){
+		Sequence sq = DOTween.Sequence();
+		sq.Append(transform.DOScale(new Vector2(1.25f, 1.25f), 0.05f));
+		sq.Append(transform.DOScale(new Vector2(1f, 1f), 0.05f));
+	}
+
+	void meatfill(char letter){
+		fill.text += letter;
+		managers.setFill (1);
+	}
+}
---
+++ Assets/meatfight/mf_manager.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class mf_manager : MonoBehaviour {
+	[SerializeField] GameObject fighters;
+	[SerializeField] GameObject[] hpbar;
+	private Animator fighteranim;
+
+	private float probability = 0.01f;
+
+	private float punchtime = 3f; //3 second long punchtime
+	private float timeLeft;
+	private bool isPunching;
+
+	private int fillLevel;
+	private int fullLevel = 1650; //1650 how many chara it takes to fill the screen at 40 pt font and 0.6 line spacing
+	private bool isFull;
+
+	private int[] hp;
+
+	// Use this for initialization
+	void Start () {
+		fighteranim = fighters.GetComponent<Animator> ();
+
+		hp = new int[2];
+		hp [0] = 100; hp [1] = 100;
+		isPunching = false;
+
+		isFull = false;
+		fillLevel = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Random.Range(0f,1f) < probability && !isPunching){
+			Debug.Log ("puncho");
+			//puncho!!!!
+			//start punch timer, let player do the thing
+			timeLeft = punchtime;
+			isPunching = true;
+			if (true) { //Random.Range(0f,1f)<0.5){ //left guy hit
+				//play the sprite stuff
+				hp[0] -= 10;
+				fighteranim.Play ("punchr");
+			} else { //right guy hit
+				//i need sprites for this so it's not in yet --TODO
+			}
+			updatehp ();
+		}
+
+		if (isPunching) {
+			timeLeft -= Time.deltaTime;
+		}
+		if (timeLeft < 0) {
+			isPunching = false;
+			fighteranim.Play ("static");
+		}
+
+		if (isFull) {
+			//screen is full, win!!!
+			Debug.Log("win. eat.");
+		}
+		if (hp [0] < 0 || hp [1] < 0) {
+			//lose!!!
+			Debug.Log("someone fuckin died!!!");
+		}
+			
+	}
+
+	void updatehp(){
+		hpbar [0].transform.localScale = new Vector3 (hp [0] / 100f * 5, 0.5f, 1f);
+		hpbar [1].transform.localScale = new Vector3 (hp [1] / 100f * 5, 0.5f, 1f);
+	}
+
+	public void setFill(int l){
+		fillLevel = fillLevel + l;
+		if (fillLevel >= fullLevel) {
+			isFull = true;
+		}
+	}
+
+	public bool getIsPunching(){
+		return isPunching;
+	}
+}
