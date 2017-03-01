a14b28d38ba235c43a90ae142964543e23ab9e25
madebyskippy <timothys@nyu.edu>
2017-03-01 17:35:33 -0500

    floaties

---
+++ Assets/floats/floater.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class floater : MonoBehaviour {
+
+	Rigidbody2D rb;
+	SpriteRenderer sr;
+	private float magnitude = 0.5f;//100f;
+	private float time;
+	private float size;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		sr = GetComponent<SpriteRenderer> ();
+
+		time = Random.Range (5f, 15f);
+		size = Random.Range (0.5f, 1.5f);
+
+		Color temp = sr.color;
+		temp.a = 0.75f;
+		sr.color = temp;
+		DOTween.ToAlpha (()=> sr.color, x=> sr.color = x , 0.25f, Random.Range(5f,10f));
+		transform.localScale= Vector3.one * size/2f;
+		Sequence sq = DOTween.Sequence();
+		sq.Append(transform.DOScale(new Vector2(1f,1f)*size, time/2f));
+		sq.Append(transform.DOScale(new Vector2(1f,1f)*size/2f, time/2f));
+
+		InvokeRepeating ("changeVelocity", 0f, 0.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		rb.rotation = Vector3.Angle (Vector3.up, rb.velocity) * Vector3.Cross (Vector3.up, rb.velocity).normalized.z;
+	}
+
+	void changeVelocity(){
+		rb.velocity += new Vector2 (Random.Range(-1f,1f) * magnitude, Random.Range(-1f,1f) * magnitude) ;
+	}
+
+	void addForce(){
+		Vector2 direction = new Vector2 (Random.Range(-1f,1f),Random.Range(-1,1f));
+		rb.AddForce (direction * magnitude);
+	}
+}
---
+++ Assets/floats/floaterspawner.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class floaterspawner : MonoBehaviour {
+
+	[SerializeField] GameObject floater;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < 50; i++) {
+			GameObject temp = Instantiate (floater);
+			temp.transform.position = new Vector3 (Random.Range(-4f,4f), Random.Range(-4f,4f), 0f);
+			temp.GetComponent<SpriteRenderer> ().color = Random.ColorHSV(0f, 1f, 0.25f, 0.5f, 0.5f, 1f);
+			temp.GetComponent<TrailRenderer> ().startColor = temp.GetComponent<SpriteRenderer> ().color;
+			temp.GetComponent<TrailRenderer> ().endColor = temp.GetComponent<SpriteRenderer> ().color;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
