6d69f4e8efdd1f33909c3d5f6bb4733b2a803deb
madebyskippy <timothys@nyu.edu>
2017-05-05 22:31:08 -0400

    code working for whole olympic spectator game

---
+++ Assets/olympics/oly_manager.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class oly_manager : MonoBehaviour {
+
+	/*
+	 * randomly pick which sport out of maybe 3 or 5
+	 * 
+	 * randomly determine which countries are playing
+	 * randomly decide which one wins
+	 * 
+	 * make user choose at the end
+	 * 		phone comes up at the end of a friend being like "who won?!"
+	 * 		and you have to click the answer
+	 */
+
+	[SerializeField] GameObject background;
+	[SerializeField] GameObject silhouette;
+	[SerializeField] GameObject[] parents;
+
+	[SerializeField] GameObject start;
+	[SerializeField] GameObject end;
+	[SerializeField] Image[] endchoices;
+	[SerializeField] Image arrow;
+
+	[SerializeField] oly_sport sport;
+
+	List<GameObject[]> crowd;
+	int layers = 4;
+	Vector2 densityRange = new Vector2(5f,7f);
+	Vector2 rangeX = new Vector2(-10f,10f);
+	Vector2 rangeY = new Vector2(-8f,-4f);
+
+	int mode; //0 1 or 2 (enter answer) or 3 (actual endgame state)
+
+	int choice = 2;
+
+	// Use this for initialization
+	void Start () {
+		crowd = new List<GameObject[]> ();
+		for (int i = 0; i < layers; i++) {
+			GameObject[] layer = new GameObject[Random.Range ((int)densityRange.x, (int)densityRange.y+1)];
+			for (int j = 0; j < layer.Length; j++) {
+				GameObject t = Instantiate (silhouette, new Vector3 (Random.Range(rangeX.x*2f,rangeX.y*2f), Random.Range(rangeY.x,rangeY.y), 0f), Quaternion.identity);
+				if (Random.Range (0f, 1f) < 0.5f) {
+					t.GetComponent<SpriteRenderer> ().flipX = true;
+				}
+				t.transform.parent = parents [i].transform;
+				layer [j] = t;
+			}
+			crowd.Add (layer);
+		}
+		start.SetActive (true);
+		end.SetActive (false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			if (mode == 1 || mode == 3) {
+				SceneManager.LoadScene ("olympics");
+			} else {
+				mode = 1;
+				sport.setGoing (true);
+				start.SetActive (false);
+			}
+		}if (Input.GetKeyDown(KeyCode.Escape)) {
+			Application.Quit();
+		}
+			
+		float horiz = Input.GetAxis ("Horizontal");
+		float vert = Input.GetAxis ("Vertical");
+
+		if (mode == 0 || mode == 1) {
+			Vector3 t = background.transform.position;
+			t.y -= 0.1f * vert;
+			t.x += 0.1f * horiz;
+			t.y = Mathf.Clamp (t.y, -1.5f, 0.5f);
+			t.x = Mathf.Clamp (t.x, -3f, 3f);
+			background.transform.position = t;
+
+			for (int i = 0; i < parents.Length; i++) {
+				t = parents [i].transform.position;
+				t.y += 0.1f * (i + 1) * vert;
+				t.x += 0.1f * (i + 1) * horiz;
+				t.y = Mathf.Clamp (t.y, -1.5f, 3f);
+				t.x = Mathf.Clamp (t.x, -8f, 8f);
+				parents [i].transform.position = t;
+			}
+		} else if (mode == 2) {
+			//end mode, move the hand
+			if (Input.GetKeyDown (KeyCode.LeftArrow)) {
+				choice = Mathf.Max (choice - 1, 0);
+			}
+			if (Input.GetKeyDown (KeyCode.RightArrow)) {
+				choice = Mathf.Min (choice + 1, 4);
+			}
+			if (Input.GetKeyDown (KeyCode.UpArrow)) {
+				enterchoice ();
+			}
+			arrow.GetComponent<RectTransform> ().localPosition = endchoices [choice].GetComponent<RectTransform> ().localPosition;
+		} else {
+		}
+	}
+
+	public void endRace(){
+		mode = 2;
+		end.SetActive (true);
+		setupendmenu ();
+	}
+
+	void setupendmenu(){
+		choice = 2;
+		GameObject[] choices = sport.ath ();
+		for (int i = 0; i < choices.Length; i++) {
+			endchoices[i].sprite = choices [i].GetComponent<SpriteRenderer> ().sprite;
+//			choices [i].transform.position = new Vector3(-5f + i*2f,2f,0f);
+		}
+	}
+
+	void enterchoice(){
+		mode = 3;
+		if (choice == sport.getwinner ()) {
+			arrow.color = Color.green;
+			//waooo u were rite
+		} else {
+			arrow.color = Color.red;
+			//u lose sucker
+		}
+	}
+}
---
+++ Assets/olympics/oly_sport.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class oly_sport : MonoBehaviour {
+
+	[SerializeField] oly_manager manager;
+	[SerializeField] Sprite[] flags;
+	[SerializeField] GameObject aprefab;
+
+	string[] countries = {"AFGHANISTAN", "ALBANIA", "ALGERIA", "AMERICAN SAMOA", "ANDORRA", "ANGOLA", "ANTIGUA AND BARBUDA",
+		"ARGENTINA", "ARMENIA", "ARUBA", "AUSTRALIA", "AUSTRIA", "AZERBAIJAN", "BAHAMAS", "BAHRAIN", "BANGLADESH", "BARBADOS",
+		"BELARUS", "BELGIUM", "BELIZE", "BENIN", "BERMUDA", "BHUTAN", "BOLIVIA", "BOSNIA AND HERZEGOVINA", "BOTSWANA", "BRAZIL",
+		"BRUNEI DARUSSALAM", "BULGARIA", "BURKINA FASO", "BURUNDI", "CAMBODIA", "CAMEROON", "CANADA", "CAPE VERDE", "CAYMAN ISLANDS",
+		"CENTRAL AFRICAN REPUBLIC", "CHAD", "CHILE", "CHINESE TAIPEI", "COLOMBIA", "COMOROS", "CONGO", "COOK ISLANDS", "COSTA RICA",
+		"CÔTE D'IVOIRE", "CROATIA", "CUBA", "CYPRUS", "CZECH REPUBLIC", "DEMOCRATIC PEOPLE'S REPUBLIC OF KOREA", "DEMOCRATIC REPUBLIC OF THE CONGO",
+		"DENMARK", "DJIBOUTI", "DOMINICA", "DOMINICAN REPUBLIC", "ECUADOR", "EGYPT", "EL SALVADOR", "EQUATORIAL GUINEA", "ERITREA", "ESTONIA", "ÉTHIOPIE",
+		"FEDERATED STATES OF MICRONESIA", "FIJI", "FINLAND", "FRANCE", "GABON", "GAMBIA", "GEORGIA", "GERMANY", "GHANA", "GREAT BRITAIN", "GREECE",
+		"GRENADA", "GUAM", "GUATEMALA", "GUINEA", "GUINEA-BISSAU", "GUYANA", "HAITI", "HONDURAS", "HONG-KONG", "HUNGARY", "ICELAND", "ÎLES MARSHALL", "INDIA",
+		"INDONESIA", "IRAQ", "IRELAND", "ISLAMIC REPUBLIC OF IRAN", "ISRAEL", "ITALY", "JAMAICA", "JAPAN", "JORDAN", "KAZAKHSTAN", "KENYA", "KIRIBATI", "KOSOVO",
+		"KUWAIT", "KYRGYZSTAN", "LAO PEOPLE'S DEMOCRATIC REPUBLIC", "LATVIA", "LEBANON", "LESOTHO", "LIBERIA", "LIBYA", "LIECHTENSTEIN", "LITHUANIA", "LUXEMBOURG",
+		"MADAGASCAR", "MALAWI", "MALAYSIA", "MALDIVES", "MALI", "MALTA", "MAURITANIA", "MAURITIUS", "MEXICO", "MONACO", "MONGOLIA", "MONTENEGRO", "MOROCCO", "MOZAMBIQUE",
+		"MYANMAR", "NAMIBIA", "NAURU", "NEPAL", "NETHERLANDS", "NEW ZEALAND", "NICARAGUA", "NIGER", "NIGERIA", "NORWAY", "OMAN", "PAKISTAN", "PALAU", "PALESTINE", "PANAMA",
+		"PAPUA NEW GUINEA", "PARAGUAY", "PEOPLE'S REPUBLIC OF CHINA", "PERU", "PHILIPPINES", "POLAND", "PORTUGAL", "PUERTO RICO", "QATAR", "REPUBLIC OF KOREA",
+		"REPUBLIC OF MOLDOVA", "ROMANIA", "RUSSIAN FEDERATION", "RWANDA", "SAINT KITTS AND NEVIS", "SAINT LUCIA", "SAMOA (UNTIL 1996 WESTERN SAMOA)", "SAN MARINO",
+		"SAO TOME AND PRINCIPE", "SAUDI ARABIA", "SENEGAL", "SERBIA", "SEYCHELLES", "SIERRA LEONE", "SINGAPORE", "SLOVAKIA", "SLOVENIA", "SOLOMON ISLANDS", "SOMALIA",
+		"SOUTH AFRICA", "SOUTH SUDAN", "SPAIN", "SRI LANKA", "ST VINCENT AND THE GRENADINES", "SUDAN", "SURINAME", "SWAZILAND", "SWEDEN", "SWITZERLAND", "SYRIAN ARAB REPUBLIC",
+		"TAJIKISTAN", "THAILAND", "THE FORMER YUGOSLAV REPUBLIC OF MACEDONIA", "TIMOR-LESTE", "TOGO", "TONGA", "TRINIDAD AND TOBAGO", "TUNISIA", "TURKEY", "TURKMENISTAN",
+		"TUVALU", "UGANDA", "UKRAINE", "UNITED ARAB EMIRATES", "UNITED REPUBLIC OF TANZANIA", "UNITED STATES OF AMERICA", "URUGUAY", "UZBEKISTAN", "VANUATU", "VENEZUELA",
+		"VIETNAM", "VIRGIN ISLANDS BRITISH", "VIRGIN ISLANDS US", "YEMEN", "ZAMBIA", "ZIMBABWE"
+	};
+
+	int endpoint = 12;
+
+	GameObject[] athletes = new GameObject[5];
+
+	int[] a = new int[5]; //keeps track of which countries are at play
+	float[] speeds = new float[5];
+
+	int winner;
+	bool done;
+
+	int maxspeed;
+
+	bool going;
+
+	// Use this for initialization
+	void Start () {
+		done = false;
+		winner = Random.Range (0, athletes.Length);
+		for (int i = 0; i < athletes.Length; i++) {
+			a [i] = Random.Range (0, flags.Length);
+			athletes [i] = Instantiate (aprefab, new Vector3 (-13.5f,4f-i*2f,6f), Quaternion.identity);
+			athletes [i].GetComponent<SpriteRenderer> ().sprite = flags [a[i]];
+			athletes [i].transform.parent = this.transform;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (going) {
+			bool alldone = true;
+			for (int i = 0; i < athletes.Length; i++) {
+				Vector3 t = athletes [i].transform.position;
+				t.x += Random.Range (0.01f, 0.1f);
+				if (t.x > endpoint && !done) {
+					done = true;
+					winner = i;
+					athletes [i].transform.position = t;
+				} else if (t.x < endpoint) {
+					athletes [i].transform.position = t;
+					alldone = false;
+				}
+			}
+			if (alldone) {
+				manager.endRace ();
+				going = false;
+			}
+		}
+	}
+
+	public void setGoing(bool g){
+		going = g;
+	}
+
+	public int getwinner(){
+		return winner;
+	}
+
+	public GameObject[] ath(){
+		return athletes;
+	}
+}
