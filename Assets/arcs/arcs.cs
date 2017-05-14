c36b041703ba38eaebae808d1b6c9272889c9cac
madebyskippy <timothys@nyu.edu>
2017-05-14 18:15:20 -0400

    sewing game juice

--- Assets/arcs/arcs.cs
+++ Assets/arcs/arcs.cs
@@ -8,7 +8,7 @@ public class arcs : MonoBehaviour {
 
 	//color all squares without touching any black dots 
 
-	[SerializeField] Vector2 start;
+	[SerializeField] Vector2 startp;
 
 	[SerializeField] GameObject l;
 	[SerializeField] GameObject circ;
@@ -22,6 +22,7 @@ public class arcs : MonoBehaviour {
 	GameObject[,] colliders;
 	GameObject[,] circles;
 	Vector2[,] grid;
+	Vector2[,] stable;
 	Vector2[,] changes;
 	int r = 10;
 	int c = 10;
@@ -37,17 +38,24 @@ public class arcs : MonoBehaviour {
 
 	bool gameover;
 	bool win;
+	bool start;
 
 	float score;
 
+	float downtime = 5*60; //will reset after 5 minutes
+	float downtimer;
+
 	// Use this for initialization
 	void Start () {
+		downtimer = 0;
 		score = 0;
 		count = 0;
-		text.text = "touch each square";
+		text.text = "use the mouse to touch each space\n\n0";
 		gameover = false;
 		win = false;
+		start = true;
 		grid = new Vector2[r, c];
+		stable = new Vector2[r, c];
 		changes = new Vector2[r, c];
 		circles = new GameObject[r, c];
 		colliders = new GameObject[r-1, c-1];
@@ -66,7 +74,8 @@ public class arcs : MonoBehaviour {
 //			lr_r [i] = t1.GetComponent<LineRenderer> ();
 
 			for (int j = 0; j < c; j++) {
-				grid [i, j] = new Vector2 (start.x + i, start.y - j);
+				grid [i, j] = new Vector2 (startp.x + i, startp.y - j);
+				stable[i,j] = new Vector2 (startp.x + i, startp.y - j);
 				changes [i, j] = Vector2.zero;
 				circles[i,j] = Instantiate (circ, grid [i, j], Quaternion.identity);
 				if (i > 0 && j < c - 1) {
@@ -105,36 +114,93 @@ public class arcs : MonoBehaviour {
 			Application.Quit();
 		}
 
+		if (Input.GetMouseButtonDown (0)) {
+			if (start) {
+				start = false;
+			}
+			if (gameover || win) {
+				SceneManager.LoadScene ("arcs");
+			}
+		}
+
 
 
 		Vector3 temp = Camera.main.ScreenToWorldPoint (Input.mousePosition);
 		temp.z = 0;
-		mouse.transform.position = temp;
+		if (temp.x != mouse.transform.position.x && temp.y != mouse.transform.position.y) {
+			//controls were touched
+//			Debug.Log(temp.x +" = "+mouse.transform.position.x+", "+temp.y+" = "+mouse.transform.position.y);
+			downtimer = 0;
+		}
+		if (!start) {
+			mouse.transform.position = temp;
+		}
 
-		for (int j = 0; j < c; j++) {
-			for (int i = 0; i < r; i++) {
-				changes [i, j] = Vector2.one * Random.Range (-1f, 1f) * increment;
-				edit (i, j, grid [i, j] + changes[i,j]);
 
-			}
+		if (!gameover && !win && !start) {
+
+			warp ();
+
+			score += Time.deltaTime;
+			text.text = "use the mouse to touch each space\n\n" + ((int)score).ToString();
+
+		}
+		if (start) {
+			text.text = "use the mouse to touch each space\n\nclick to start";
+		}
+		if (gameover) {
+			text.text = "don't touch the intersections.\n\nclick to try again";
+		}
+		if (win) {
+			text.text = "amazing. do it again\n\n" + ((int)score).ToString ();
+		}
+		if (gameover || win || start) {
+			jiggle();
 		}
 
 		showlines ();
 		placecirc ();
 		adjustpoly ();
 
-		if (!gameover && !win) {
-			
-			score += Time.deltaTime;
-			text.text = "touch each square\n\n" + ((int)score).ToString();
-
-
+		downtimer += Time.deltaTime;
+		if (downtimer > downtime) {
+			SceneManager.LoadScene ("arcs");
 		}
-		if (gameover) {
-			text.text = "don't touch the corners.\nspace to try again\n" + ((int)score).ToString ();
+	}
+
+	void jiggle(){
+		float threshold = 0.05f;
+		for (int j = 0; j < c; j++) {
+			for (int i = 0; i < r; i++) {
+				float x, y;
+				if (grid [i, j].x - stable [i, j].x > threshold) {
+					x = Random.Range (-1f, 0) * increment;
+				} else if (stable [i, j].x - grid [i, j].x > threshold) {
+					x = Random.Range (0, 1f) * increment;
+				} else {
+					x = Random.Range (-1f, 1f) * increment;
+				}
+				if (grid [i, j].y - stable [i, j].y > threshold) {
+					y = Random.Range (-1f, 0) * increment;
+				} else if (stable [i, j].y - grid [i, j].y > threshold) {
+					y = Random.Range (0, 1f) * increment;
+				} else {
+					y = Random.Range (-1f, 1f) * increment;
+				}
+				changes [i, j] = new Vector2 (x, y);
+				edit (i, j, grid [i, j] + changes[i,j]);
+
+			}
 		}
-		if (win) {
-			text.text = "amazing. do it again\n\n" + ((int)score).ToString ();
+	}
+	void warp(){
+		for (int j = 0; j < c; j++) {
+			for (int i = 0; i < r; i++) {
+//				changes [i, j] = Vector2.one * Random.Range (-1f, 1f) * increment;
+				changes[i,j] = new Vector2(Random.Range(-1f,1f),Random.Range(-1f,1f)) * increment;
+				edit (i, j, grid [i, j] + changes[i,j]);
+
+			}
 		}
 	}
 
@@ -174,10 +240,10 @@ public class arcs : MonoBehaviour {
 
 				o.GetComponent<PolygonCollider2D> ().points = v2;
 				v3 = o.GetComponent<MeshFilter> ().mesh.vertices;
-				v3 [0] = o.transform.InverseTransformPoint((Vector3)grid [i-1,	j+1]);//-Vector3.one;
-				v3 [1] = o.transform.InverseTransformPoint((Vector3)grid [i, 	j]);//-Vector3.one;
-				v3 [2] = o.transform.InverseTransformPoint((Vector3)grid [i,	j+1]);//-Vector3.one;
-				v3 [3] = o.transform.InverseTransformPoint((Vector3)grid [i-1,	j]);//-Vector3.one;
+				v3 [0] = o.transform.InverseTransformPoint((Vector3)grid [i-1,	j+1]+new Vector3(0,0,1f));//-Vector3.one;
+				v3 [1] = o.transform.InverseTransformPoint((Vector3)grid [i, 	j]+new Vector3(0,0,1f));//-Vector3.one;
+				v3 [2] = o.transform.InverseTransformPoint((Vector3)grid [i,	j+1]+new Vector3(0,0,1f));//-Vector3.one;
+				v3 [3] = o.transform.InverseTransformPoint((Vector3)grid [i-1,	j]+new Vector3(0,0,1f));//-Vector3.one;
 				o.GetComponent<MeshFilter> ().mesh.vertices = v3;
 				o.GetComponent<MeshFilter> ().mesh.triangles = new int[] {0,1,2,1,0,3};
 			}
@@ -188,10 +254,13 @@ public class arcs : MonoBehaviour {
 		if (!gameover && !win){
 			if (corner) {
 				Debug.Log ("hit");
+				col.gameObject.GetComponent<SpriteRenderer> ().enabled = true;
+				col.gameObject.transform.localScale = new Vector3 (0.5f, 0.5f, 0.5f);
+				col.gameObject.transform.GetChild (0).gameObject.SetActive (true);
 				if (!gameover) {
-					Camera.main.backgroundColor = new Color (0.9f,0.9f,0.9f);
+					Camera.main.backgroundColor = new Color (0.68f,0.68f,0.68f);
 					gameover = true;
-					screenshot ();
+					whitelines ();
 				}
 			} else {
 				if (!col.GetComponent<arcpoly> ().gethit ()) {
@@ -201,16 +270,20 @@ public class arcs : MonoBehaviour {
 					col.GetComponent<MeshRenderer> ().material = m;
 					if (count >= ((r - 1) * (c - 1)) && !win) {
 						win = true;
-						for (int i = 0; i < r; i++) {
-							lr_r [i].startColor = Color.white;
-							lr_r [i].endColor = Color.white;
-							lr_c [i].startColor = Color.white;
-							lr_c [i].endColor = Color.white;
-						}
+						whitelines ();
 						screenshot ();
 					}
 				}
 			}
+		}
+	}
+
+	void whitelines(){
+		for (int i = 0; i < r; i++) {
+			lr_r [i].startColor = Color.white;
+			lr_r [i].endColor = Color.white;
+			lr_c [i].startColor = Color.white;
+			lr_c [i].endColor = Color.white;
 		}
 	}
 
